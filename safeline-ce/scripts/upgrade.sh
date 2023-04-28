d002ccbdc66238cbe63226b6c80e4516639dcd39
phxa1 <phxaz010@gmail.com>
2023-04-28 14:46:52 +0800

    feat(safeline-ce): add safeline-ce app(v1.2.0)

---
+++ safeline-ce/scripts/upgrade.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# When your app need upgrade this script will be called after command 'docker compose up' 
+# done base on the new docker-compose.yaml file.
+# Write your logic here.
