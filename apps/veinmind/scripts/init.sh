bf564e7c086e99029879b120a6c18c62a3bbdd40
DVKunion <dvkunion@gmail.com>
2023-05-16 15:52:35 +0800

    fix(apps/veinmind): veinmind token env error

--- apps/veinmind/scripts/init.sh
+++ apps/veinmind/scripts/init.sh
@@ -2,4 +2,4 @@
 
 # When your app is installed this script will be called after command 'docker compose up' done.
 # Write your logic here.
-export RIVERS_TOKEN=`curl -s -k --location --request GET -H "X-Ca-Token: {{variables.token.value}}" -H "x-rivers-user-id:1" https://veinmind.rivers.chaitin.cn/agent/install_cmd\?source_type\=1\&group_id\=0\&deploy_method\=1 | awk -F"token=|'" '{print $3}'`
+echo RIVERS_TOKEN=`curl -s -k --location --request GET -H "X-Ca-Token:  {{variables.token.value}}" -H "x-rivers-user-id:1" https://veinmind.rivers.chaitin.cn/agent/install_cmd\?source_type\=1\&group_id\=0\&deploy_method\=1 | awk -F"token=|'" '{print $3}'` >> .env
