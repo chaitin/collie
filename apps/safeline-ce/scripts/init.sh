fd94b03ade6cf7f953e82937f76fca029fe2711e
Monster <naocanmonster@gmail.com>
2023-06-27 10:53:30 +0800

    修改雷池安装脚本

--- apps/safeline-ce/scripts/init.sh
+++ apps/safeline-ce/scripts/init.sh
@@ -3,4 +3,9 @@
 # When your app is installed this script will be called after command 'docker compose up' done.
 # Write your logic here.
 
+echo "SAFELINE_DIR=$(pwd)" >> .env
+echo "IMAGE_TAG=latest" >> .env
+echo "MGT_PORT=9443" >> .env
 echo "POSTGRES_PASSWORD=$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 32)" >> .env
+echo "REDIS_PASSWORD=$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 32)" >> .env
+echo "SUBNET_PREFIX=169.254.0" >> .env
--- apps/safeline-ce/scripts/upgrade.sh
+++ apps/safeline-ce/scripts/upgrade.sh
@@ -3,3 +3,11 @@
 # When your app need upgrade this script will be called after command 'docker compose up' 
 # done base on the new docker-compose.yaml file.
 # Write your logic here.
+
+
+grep "SAFELINE_DIR" ".env" > /dev/null || echo "SAFELINE_DIR=$(pwd)" >> .env
+grep "IMAGE_TAG" ".env" > /dev/null || echo "IMAGE_TAG=latest" >> .env
+grep "MGT_PORT" ".env" > /dev/null || echo "MGT_PORT=9443" >> .env
+grep "POSTGRES_PASSWORD" ".env" > /dev/null || echo "POSTGRES_PASSWORD=$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 32)" >> .env
+grep "REDIS_PASSWORD" ".env" > /dev/null || echo "REDIS_PASSWORD=$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 32)" >> .env
+grep "SUBNET_PREFIX" ".env" > /dev/null || echo "SUBNET_PREFIX=169.254.0" >> .env
